75b467d3a1842f7a98b053d3a04519913ea9556b
vagrant <vagrant@homestead>
2016-09-21 07:22:15 +0000

    User module update 2

---
+++ resources/assets/js/user/User.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row">
+        <div class="col-md-2">
+            <div class="panel panel-default panel-flush">
+                <div class="panel-heading">
+                    Actions
+                </div>
+                <div class="panel-body">
+
+                    <ul class="nav app-tabs-stacked">
+                        <li >
+                            <a v-link="{ path: '/user/create' }">
+                                <i class="fa fa-btn fa-fw fa-lightbulb-o"></i>Create User
+                            </a>
+                        </li>
+                        <li >
+                            <a v-link="{ path: '/user/index' }">
+                                <i class="fa fa-btn fa-fw fa-list"></i>View Users
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-10">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>

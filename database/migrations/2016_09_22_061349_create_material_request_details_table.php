aaae3f4b5bc6713aa5bd945441aecf99abaa2752
Chamith <chamith@elsteel.com>
2016-09-22 06:16:14 +0000

    User module + material requests

---
+++ database/migrations/2016_09_22_061349_create_material_request_details_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMaterialRequestDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('material_request_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('material_request_details');
+    }
+}
